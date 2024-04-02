cc2397747f02c589c3ea41980681c487af6914a4
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-02 15:00:40 +0200

    spring_project

---
+++ src/test/java/backendw4d1/w4d1/W4D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package backendw4d1.w4d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W4D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
